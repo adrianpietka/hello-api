321808261065a69de4ce0a05759e9c66cfb0c8ad
Adrian Pietka <adrian@pietka.me>
2019-04-01 15:09:20 +0200

    Change AWS Region and remove formating date

--- run-awslogs.sh
+++ run-awslogs.sh
@@ -2,10 +2,9 @@
 
 echo "Run container with AWS CloudWatch Logs..."
 docker run --log-driver=awslogs \
-    --log-opt awslogs-region=us-west-1b \
+    --log-opt awslogs-region=eu-west-1b \
     --log-opt awslogs-group=hello-api-service-group \
     --log-opt awslogs-stream=hello-api-service-stream \
-    --log-opt awslogs-datetime-format='\[%Y-%m-%dT%H:%M:%S%z\]' \
     --log-opt awslogs-create-group=true \
     -i \
     -p 3000:3000 \
